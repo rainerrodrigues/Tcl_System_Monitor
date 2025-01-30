a8eab9ce60b367a8ac2c3a4af371a5fe707ca01c
rainerrodrigues <rainerrodrigues16@gmail.com>
2025-01-30 02:24:40 +0000

    Created files and created procedure to get CPU usage

---
+++ system_monitor.tcl
@@ -0,0 +1,26 @@
+package require Tclx;
+
+#Configuration: Set Thresholds for alerts
+set CPU_THRESHOLD 80; #CPU usage threshold in percentage
+set MEMORY_THRESHOLD 90;# Memory usage threshold in percentage
+set DISK_THRESHOLD 90; #Disk usage threshold in percentagge
+
+# Procedure to get CPU usage
+proc get_cpu_usuage {} {
+	set total_usage 0
+	set cpu_info [exec cat /proc/stat]
+	set lines [split $cpu_info "\n"]
+	foreach line $lines {
+		if {[string match "cpu *" $line]} {
+			set values [regexp -all -inline {\d+} $line]
+			set user [lindex $values 0]
+			set nice [lindex $values 1]
+			set system [lindex $values 2]
+			set idle [lindex $values 3]
+			set total [expr {$user + $nice + $system + $idele}]
+			set usage [expr {100 - ($idle * 100.0 / $total)}]
+			return [format "%.2f" $usage]
+		}
+	}
+	return 0
+}
